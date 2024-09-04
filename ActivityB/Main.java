ae130ebaf40bcd94be486ab94c0b19abb05f1299
Nelman25 <jonelvillaver735@gmail.com>
2024-09-04 19:37:58 +0800

    Activity B done

---
+++ ActivityB/Main.java
@@ -0,0 +1,38 @@
+package ActivityB;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    while(true) {
+      System.out.print("Enter your score: ");
+      int score = input.nextInt();
+      System.out.println(gpaChecker(score));;
+    }
+  }
+  static String gpaChecker(int score) {
+    String text = "";
+    if(score >= 96 && score <= 100) {
+      text = "Your GPA is: 4.0";
+    } else if(score >= 90 && score <= 95) {
+      text = "Your GPA is: 3.5";
+    } else if(score >= 84 && score <= 89) {
+      text = "Your GPA is: 3.0";
+    } else if(score >= 78 && score <= 83) {
+      text = "Your GPA is: 2.5";
+    } else if(score >= 72 && score <= 77) {
+      text = "Your GPA is: 2.0";
+    } else if(score >= 66 && score <= 71) {
+      text = "Your GPA is: 1.5";
+    } else if(score >= 60 && score <= 65) {
+      text = "Your GPA is: 1.0";
+    } else if(score < 60) {
+      text = "You need to repeat the subject.";
+    } else {
+      text = "Please enter a valid score.";
+    }
+    return text;
+  }
+}
